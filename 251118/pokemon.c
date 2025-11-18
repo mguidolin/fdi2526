888956066946a2e18242f0fc0ffefd9db1dd1ce2
Mattia Guidolin <mattia.guidolin@gmail.com>
2025-11-18 15:09:17 +0100

    Add 18/11/25 lesson

---
+++ 251118/pokemon.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// #define DEBUG
+
+enum Ball
+{
+  POKEBALL = 1,
+  GREATBALL,
+  ULTRABALL
+};
+
+int main()
+{
+  srand(time(NULL));
+
+  enum Ball ball = POKEBALL;
+  unsigned int attempts = 0;
+  unsigned int catch_rate_base = 20;
+  unsigned int catch_rate_total = 0;
+
+  unsigned int roll = 0;
+
+  printf("Suddenly a wild Pikachu appears!\n");
+
+  do
+  {
+    roll = (rand() % 100) + 1;
+
+#ifdef DEBUG
+    printf("[ roll = %u ]\n", roll);
+#endif
+
+    printf("\nChoose a ball:\n");
+    printf("1. Pokeball\n");
+    printf("2. Great ball\n");
+    printf("3. Ultra ball\n");
+    printf("Choice: ");
+    scanf("%d", &ball);
+
+    switch (ball)
+    {
+    case POKEBALL:
+      catch_rate_total = (unsigned)(catch_rate_base * 1);
+      break;
+    case GREATBALL:
+      catch_rate_total = (unsigned)(catch_rate_base * 1.5);
+      break;
+    case ULTRABALL:
+      catch_rate_total = (unsigned)(catch_rate_base * 2);
+      break;
+    default:
+      printf("Wrong ball!\n");
+      catch_rate_total = 0;
+      break;
+    }
+
+    attempts++;
+
+    if (roll <= catch_rate_total)
+    {
+      printf("\nYou caught it!\n");
+      printf("It took %u attempts!\n", attempts);
+    }
+    else
+    {
+      printf("\nThe pokemon escaped!\n");
+    }
+  } while (roll > catch_rate_total);
+
+  return 0;
+}
